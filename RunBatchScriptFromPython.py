49e47aee7b2329853a2ae66e839b20488658216b
rhaglennwr <rhaglennwr@gmail.com>
2022-06-28 17:05:24 -0700

    added more output

--- RunBatchScriptFromPython.py
+++ RunBatchScriptFromPython.py
@@ -2,7 +2,7 @@ import time
 import subprocess as sub
 
 start_time = time.time()
-print("ping google.com once")
+print("ping google.com once in myBatchFile0.bat")
 print ("start 1")
 sub.call([r'C:\Users\ibike\Paul\RunBatchFromPython\myBatchFile0.bat'])
 print("finish 1")
@@ -11,7 +11,7 @@ print("--- %s seconds ---" % (time.time() - start_time))
 print()
 
 start_time = time.time()
-print("ping google.com twice")
+print("ping google.com twice in myBatchFile1.bat")
 print ("start 2")
 sub.call([r'C:\Users\ibike\Paul\RunBatchFromPython\myBatchFile1.bat'])
 print("finish 2")
@@ -20,7 +20,7 @@ print("--- %s seconds ---" % (time.time() - start_time))
 print()
 
 start_time = time.time()
-print("ping google.com thrice")
+print("ping google.com thrice in myBatchFile2.bat")
 print ("start 3")
 sub.call([r'C:\Users\ibike\Paul\RunBatchFromPython\myBatchFile2.bat'])
 print("finish 3")
